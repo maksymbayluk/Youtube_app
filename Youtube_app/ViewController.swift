5d31130a9b32f6ef077760103118619d2fe6a771
Maxym Bayluk <maksym@MacBook-Pro-Maxym-4.local>
2020-08-27 15:23:26 +0300

    initial xcode project creation

---
+++ Youtube_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Youtube_app
+//
+//  Created by Maxym Bayluk on 27.08.2020.
+//  Copyright © 2020 Maxym Bayluk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
